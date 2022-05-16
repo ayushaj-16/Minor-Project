7b832506c9e3f5c54dd6085d11b169f01aa753a1
Ayush Jaiswal <jaiswalayush131313@gmail.com>
2022-05-16 20:54:24 +0530

    Added details of additional algo

--- assets/js/dataEntries.js
+++ assets/js/dataEntries.js
@@ -186,6 +186,8 @@ var modal4 = document.getElementById("myModal4");
 var modal5 = document.getElementById("myModal5");
 var modal6 = document.getElementById("myModal6");
 var modal7 = document.getElementById("myModal7");
+var modal8 = document.getElementById("myModal8");
+var modal9 = document.getElementById("myModal9");
 
 // Get the button that opens the modal
 var btn1 = document.getElementById("myBtn1");
@@ -195,6 +197,8 @@ var btn4 = document.getElementById("myBtn4");
 var btn5 = document.getElementById("myBtn5");
 var btn6 = document.getElementById("myBtn6");
 var btn7 = document.getElementById("myBtn7");
+var btn8 = document.getElementById("myBtn8");
+var btn9 = document.getElementById("myBtn9");
 
 // Get the <span> element that closes the modal
 var span1 = document.getElementsByClassName("close1")[0];
@@ -204,6 +208,8 @@ var span4 = document.getElementsByClassName("close4")[0];
 var span5 = document.getElementsByClassName("close5")[0];
 var span6 = document.getElementsByClassName("close6")[0];
 var span7 = document.getElementsByClassName("close7")[0];
+var span8 = document.getElementsByClassName("close8")[0];
+var span9 = document.getElementsByClassName("close9")[0];
 
 
 // When the user clicks on the button, open the modal
@@ -228,6 +234,12 @@ btn6.onclick = function() {
 btn7.onclick = function() {
   modal7.style.display = "block";
 }
+btn8.onclick = function() {
+  modal8.style.display = "block";
+}
+btn9.onclick = function() {
+  modal9.style.display = "block";
+}
 
 // When the user clicks on <span> (x), close the modal
 span1.onclick = function() {
@@ -250,6 +262,12 @@ span6.onclick = function() {
 }
 span7.onclick = function() {
   modal7.style.display = "none";
+}
+span8.onclick = function() {
+  modal8.style.display = "none";
+}
+span9.onclick = function() {
+  modal9.style.display = "none";
 }
 
 
@@ -274,5 +292,11 @@ window.onclick = function(event) {
   }
   if (event.target == modal7) {
     modal7.style.display = "none";
+  }
+  if (event.target == modal8) {
+    modal8.style.display = "none";
+  }
+  if (event.target == modal8) {
+    modal9.style.display = "none";
   }
 }
